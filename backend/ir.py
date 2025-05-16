8a57772311ffa2ab4c9caaa1da122cad4539d9e8
Lohit P Talavar <128409310+lohitpt252003@users.noreply.github.com>
2025-05-16 12:50:34 +0530

    ir.py

--- backend/ir.py
+++ backend/ir.py
@@ -7,26 +7,43 @@ def ir(ast):
     for node in nodes:
         if node['type'] == 'VariableDecl':
             name = node['name']
-            params = node['params']
+            params = node.get('params', {})
             data_type = node['data_type']
-            if data_type == 'int':
-                if not params:
-                    value = generate_integer()
-                else:
-                    value = generate_integer(params['lower'], params['upper'])
-            elif data_type == 'float':
-                if not params:
-                    value = generate_float()
-                else:
-                    value = generate_float(params['lower'], params['upper'])
-            elif data_type == 'char':
-                value = generate_char()
-            elif data_type == 'string':
-                value = generate_string()
+            value = None
 
-        output_arr.push_back({
-            'name' : name,
-            'value' : value
+            if data_type == 'int':
+                # Handle integer constraints
+                if 'lower' in params and 'upper' in params:
+                    value = generate_integer(
+                        lower=params['lower']['value'],
+                        upper=params['upper']['value']
+                    )
+                else:
+                    value = generate_integer()
+
+            elif data_type == 'float':
+                # Handle float constraints and precision
+                precision = params.get('precision', {}).get('value', 6) if 'precision' in params else 6
+                lower = params.get('lower', {}).get('value', 0)
+                upper = params.get('upper', {}).get('value', 1e18)
+                value = generate_float(lower, upper, precision)
+
+            elif data_type == 'char':
+                # Handle character set constraints
+                charset = params.get('charset', 'lower+upper+digit+special').split('+')
+                value = generate_char(charset)
+
+            elif data_type == 'string':
+                # Handle string length and character set
+                size = params.get('size', {}).get('value', 10)
+                charset = params.get('charset', 'lower+upper+digit+special').split('+')
+                value = generate_string(size, charset)
+
+            output_arr.append({
+                'name': name,
+                'type': data_type,
+                'value': value,
+                'constraints': params
             })
-        
-        return output_arr
+
+    return output_arr
